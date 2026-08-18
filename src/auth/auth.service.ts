8e0f12087b9bdbe114218f8059731e70b9654e50
Vansh <vanshchoudhary2744@gmail.com>
2026-08-18 12:45:45 +0530

    hashing the password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { SignupDto } from './dto/signup.dto';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
 
-  signUp(signupDto: SignupDto) {
-    return this.userService.signUp(signupDto);
+  async signUp(signupDto: SignupDto) {
+    const hashPassword = await bcrypt.hash(signupDto.password, 10);
+    return this.userService.signUp({ ...signupDto, password: hashPassword });
   }
 }
